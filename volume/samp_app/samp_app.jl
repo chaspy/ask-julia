82968064b9fbc630374edc7f9ef2db24c5101b59
ariririri <fatal.t.h.f.flydream.bamboo@gmail.com>
2019-02-04 09:23:29 +0900

    support searchligt but sql not works

--- volume/samp_app/samp_app.jl
+++ volume/samp_app/samp_app.jl
@@ -3,6 +3,9 @@ import Genie.Router: route
 Genie.config.run_as_server = true
 Genie.REPL.load_app()
 
+using SearchLight
+SearchLight.Configuration.load_db_connection()
+
 route("/hello") do
   "Hello world!"
 end
